e9449368dcb142e9a77104daa6ccdae7c25c762b
Jurek Grinco <64655439+MysticCrab@users.noreply.github.com>
2024-04-10 08:09:09 +0200

    Second Java HW
    +-%/* 
    string

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,");
-        System.out.println("my");
-        System.out.println("name");
-        System.out.println("is");
-        System.out.println("!");
-        System.out.print("Hello, my name is !");
-    }
+        byte a = 56;
+        short b = 12678;
+        double c = 17.45677;
+        System.out.println(a+b);
+        System.out.println(c-b);
+        System.out.println(c/a);
+        System.out.println(b*a);
+        System.out.println(b%c);;
+String str01 = "345";
+        System.out.println(str01.charAt(0) + "," + str01.charAt(1) + "," + str01.charAt(2));
+}
 }
