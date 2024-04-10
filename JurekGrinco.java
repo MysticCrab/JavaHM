15475b195590aafbabc2ed96505d33e7e2b3006e
Jurek Grinco <64655439+MysticCrab@users.noreply.github.com>
2024-04-10 08:17:38 +0200

    Second Java HM
    +-*%/
    string

---
+++ JurekGrinco.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 56;
+        short b = 12678;
+        double c = 17.45677;
+        System.out.println(a+b);
+        System.out.println(c-b);
+        System.out.println(c/a);
+        System.out.println(b*a);
+        System.out.println(b%c);;
+        String str01 = "345";
+        System.out.println(str01.charAt(0) + "," + str01.charAt(1) + "," + str01.charAt(2));
+    }
+}
